a17aa74a7cfa1b443b01ef5a207486d050eec32d
heaventhaler <133958168+heaventhaler@users.noreply.github.com>
2026-02-07 19:06:56 +0100

    display characters

--- frontend/src/app/services/book.ts
+++ frontend/src/app/services/book.ts
@@ -1,12 +1,27 @@
 export interface Book {
   bookId: number;
   title: string;
-  author?: string;
-  genre: string;
+  author: string; // im Backend NICHT nullable
+  genre?: string;
   description?: string;
+  pages?: number;
+  characters: Character[];
 }
-
 export interface CreateBook {
   title: string;
   author?: string;
+}
+
+export interface Character {
+  characterId: number;
+  name: string;
+  description?: string;
+  profession?: string;
+  species?: Species;
+}
+
+export interface Species {
+  speciesId: number;
+  name: string;
+  description?: string;
 }
